f9619346908d0dbe56ff42485f2292cb2294662e
Robin Cartier <rcartier@wanadev.fr>
2019-05-02 14:05:06 +0200

    editor html and css base

--- src/App/Editor/index.js
+++ src/App/Editor/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import "./Editor.css"
+
+class InputEditor extends React.Component {
+  render() {
+    return (
+      <p>Type your note ...</p>
+    );
+  }
+}
 
 function Editor() {
   return (
-    <p>
-        Editor
-    </p>
+    <React.Fragment>
+      <div contenteditable="true">
+        <InputEditor/>
+      </div>
+    </React.Fragment>
   );
 }
 
