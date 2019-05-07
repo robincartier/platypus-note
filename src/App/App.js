bea7194326d97c0dfa34a8d8b67cffa5db38ba49
Robin Cartier <rcartier@wanadev.fr>
2019-05-07 13:45:24 +0200

    add grommet base

--- src/App/App.js
+++ src/App/App.js
@@ -1,31 +1,85 @@
 import React from 'react';
-import './App.css';
+import {
+  Box,
+  Grommet, 
+} from "grommet";
+// import './App.css';
 import Editor from './Editor';
 import Header from './Header';
 import List from './List';
 import Menu from './Menu';
 
+const theme = {
+  global: {
+    colors: {
+      brand: '#474443',
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header/>
-      </header>
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            <Header/>
+          </AppBar>
 
-      <div className="Page">
+          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
 
-        <nav className="Menu">
-          <Menu/>
-        </nav>
+            <Box 
+              width='small'
+              background='light-2'
+              elevation='small'
+              align='center'
+              justify='center' 
+            >
+              <Menu/>
+            </Box>
 
-        <div className="Note-list">
-          <List/>
-        </div>
+            <Box direction='row' flex>
+              <Box 
+                width='medium'
+                elevation='small'
+                align='center'
+                justify='center' 
+              >
+                <List/>
+              </Box>
 
-        <div className="Editor">
-          <Editor/>
-        </div>
-      </div>
+              <Box 
+                flex
+                elevation='small'
+                align='center'
+                justify='center' 
+              >
+                <Editor/>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+       
+      </Grommet>
     </div>
   );
 }
