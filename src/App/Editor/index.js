bb9bff09eca40b87286646ac275559185c499afb
Robin Cartier <rcartier@wanadev.fr>
2019-05-14 14:16:08 +0200

    style

--- src/App/Editor/index.js
+++ src/App/Editor/index.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Box, Markdown, TextArea, Text} from "grommet";
 import "./Editor.css";
 import marked from "marked";
 import styled from "styled-components";
 
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    pedantic: false,
-    gfm: true,
-    tables: true,
-    breaks: false,
-    sanitize: false,
-    smartLists: true,
-    smartypants: false,
-    xhtml: false
-});
+// marked.setOptions({
+//     renderer: new marked.Renderer(),
+//     pedantic: false,
+//     gfm: true,
+//     tables: true,
+//     breaks: false,
+//     sanitize: false,
+//     smartLists: true,
+//     smartypants: false,
+//     xhtml: false
+// });
 
 const StyledPre = styled.pre`
   background-color: #7d4cdb;
@@ -22,9 +22,12 @@ const StyledPre = styled.pre`
 
 function Paragraph(props) {
     return (
-        <Box onClick={props.onClick}>
+        // TODO: box with the size of the MD Text inside
+        <Box onClick={props.onClick}
+            height={props.height ? props.height + "px" : "auto"}
+        >
             { (props.isSelected) ?
-                <TextArea value={props.value} onChange={props.onChange} /> :
+                <TextArea value={props.value} onChange={props.onChange} fill={true} plain resize={false}/> :
                 <Markdown components={{ pre: StyledPre }} >{props.value}</Markdown>
             }
         </Box>
@@ -34,8 +37,11 @@ class InputEditor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lines: ["test", "test2", "test\n\ntest\n\naaaaaa", "test2", "test", "test2", "test", "test2", "test", "test2"],
+            lines: ["# Et bona frustra meritis\n## Dentes in tellure",
+                "Lorem markdownum *dato*; docti *grege cognoscendus* ullum. Et hic, mirabere\nvero; accipe mente, quod nil. Precibusque patris leniat iniqua exsultantemque\n> Inque et coruscant quae potentia adventum **leves**, mens caecae, collabitur.\n> Nomenque potentia orbis, et sumus praecordiaque haud.", "__test__\n\ntest\n\naaaaaa", "test2", "test", "test2", "test", "test2", "test", "test2",
+                "1. Clangore silva epota neque labitur\n2. Modo malis\n3. Cupidine peti undis Xanthos parum", "_test2_"],
             editedLine: null,
+            heights: []
         };
         this.onChange = this.onChange.bind(this);
     }
@@ -47,17 +53,21 @@ class InputEditor extends React.Component {
         this.setState({lignes: lines});
     }
 
-    onClick(i) {
-        this.setState({editedLine: i});
+    onClick(event, i) {
+        if (this.state.editedLine === i) return;
+        const heights = this.state.heights;
+        heights[i] = event.currentTarget.offsetHeight;
+        this.setState({editedLine: i, heights: heights});
     }
 
     preRender() {
         return this.state.lines.map((line, i) => {
             return <Paragraph
-                onClick={() => this.onClick(i)}
+                onClick={(event) => this.onClick(event, i)}
                 onChange={this.onChange}
                 key={i}
                 value={line}
+                height={this.state.heights[i]}
                 isSelected = {this.state.editedLine === i}
             />;
         });
@@ -65,20 +75,36 @@ class InputEditor extends React.Component {
 
     render() {
         return (
-            <div className="editable">
+            <Box>
                 <p>Type your note ...</p>
                 {this.preRender()}
-            </div>
+            </Box>
         );
     }
 }
 
 function Editor() {
     return (
-        <React.Fragment>
-            <p>Editor</p>
-            <InputEditor/>
-        </React.Fragment>
+        <Box
+            flex
+            elevation='small'
+        >
+            <Box
+                align="center"
+            >
+                <p>Editor</p>
+            </Box>
+            <Box
+                direction='column'
+                flex
+                overflow={{ horizontal: "hidden", vertical: "scroll" }}
+                height="small"
+                pad="medium"
+                // border={{ size: "medium", style: "solid" }}
+            >
+                <InputEditor/>
+            </Box>
+        </Box>
     );
 }
 
